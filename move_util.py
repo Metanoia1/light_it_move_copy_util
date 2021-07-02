8b88eccea82249e90033db7528064997bf439c72
metanoia <meta3479@gmail.com>
2021-07-02 11:11:47 +0300

    version 1.4

--- move_util.py
+++ move_util.py
@@ -1,6 +1,7 @@
 import os
 import shutil
 import threading
+from typing import List
 from argparse import ArgumentParser
 
 
@@ -24,24 +25,38 @@ class FileManager(threading.Thread):
         self.start()
 
 
-def get_thread(threads_amount):
+def get_thread(threads_amount: int):
     if FileManager.count < threads_amount:
         return FileManager()
     return shutil.move
 
 
-def move_files(from_, to_, threads_amount=1):
+def move_files(from_: str, to_: str, threads_amount=1) -> None:
     thread = get_thread(threads_amount)
+
     try:
         files_list = os.listdir(from_)
         thread(from_, to_)
+
         for file_name in files_list:
             move_files(f"{from_}/{file_name}", to_, threads_amount)
+
     except NotADirectoryError:
         thread(from_, to_)
 
 
-def main():
+def run_move_files(from_: List[str], to_: str, threads_amount: int) -> None:
+    for arg in from_:
+        move_files(arg, to_, max(threads_amount, 1))
+
+
+def get_threads_amount(arg_threads) -> int:
+    if arg_threads:
+        return max(arg_threads, 1)
+    return 1
+
+
+def main() -> None:
     parser = ArgumentParser()
     parser.add_argument("-o", "--operation", help="use with `move` argument")
     parser.add_argument("-f", "--FROM", nargs="*")
@@ -50,8 +65,7 @@ def main():
     args = parser.parse_args()
 
     if args.operation == "move":
-        for arg in args.FROM:
-            move_files(arg, args.TO, args.threads)
+        run_move_files(args.FROM, args.TO, get_threads_amount(args.threads))
 
 
 if __name__ == "__main__":
