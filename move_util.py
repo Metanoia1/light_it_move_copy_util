6afa20296257a5787bcdecfe9d6b3e0adf27fbc1
metanoia <meta3479@gmail.com>
2021-07-02 16:33:09 +0300

    version 1.7

--- move_util.py
+++ move_util.py
@@ -1,21 +1,23 @@
 import os
 import shutil
-import threading
 from typing import List
+from threading import Thread
 from argparse import ArgumentParser
+from collections.abc import Callable
+from abc import ABCMeta, abstractmethod
 
 
-class FileManager(threading.Thread):
+class MoveFileThread(Thread):
 
-    count = 1
+    count = 0
 
-    def __init__(self, from_=None, to_=None):
+    def __init__(self, from_=None, to_=None) -> None:
         super().__init__()
         self.__class__.count += 1
         self.from_ = from_
         self.to_ = to_
 
-    def run(self):
+    def run(self) -> None:
         try:
             shutil.move(self.from_, self.to_)
         except FileNotFoundError:
@@ -23,41 +25,69 @@ class FileManager(threading.Thread):
 
         self.__class__.count -= 1
 
-    def __call__(self, from_, to_):
+    def __call__(self, from_: str, to_: str) -> None:
         self.from_ = from_
         self.to_ = to_
         self.start()
 
 
-def get_thread(threads_amount: int):
-    if FileManager.count <= threads_amount:
-        return FileManager()
-    return shutil.move
+class FileManager(metaclass=ABCMeta):
+    @abstractmethod
+    def main(self, from_: str, to_: str) -> None:
+        """Move or Copy logic implementation"""
+
+    @abstractmethod
+    def run_main(self, from_: List[str], to_: str, threads_amount=1) -> None:
+        """The self.main running method"""
 
 
-def move_files(from_: str, to_: str, threads_amount=1) -> None:
-    thread = get_thread(threads_amount)
+class FileMover(FileManager):
+    def __init__(self, thread_class: Thread, func: Callable) -> None:
+        self.from_ = None
+        self.to_ = None
+        self._threads_amount = 1
+        self._thread_class = thread_class
+        self._func = func
 
-    try:
-        files_list = os.listdir(from_)
-        thread(from_, to_)
+    def _get_thread(self) -> Callable:
+        threads_running = self._thread_class.count
+        threads_amount = self._threads_amount
+        if threads_running <= threads_amount and threads_amount != 1:
+            return self._thread_class()
+        return self._func
 
-        for file_name in files_list:
-            try:
-                move_files(f"{from_}/{file_name}", to_, threads_amount)
-            except FileNotFoundError:
-                pass
+    def _run_thread(self, from_: str, to_: str, thread: Thread) -> None:
+        if isinstance(thread, self._thread_class):
+            thread(from_, to_)
+            thread.join()
+        else:
+            thread(from_, to_)
 
-    except NotADirectoryError:
-        thread(from_, to_)
+    def main(self, from_: str, to_: str) -> None:
+        thread = self._get_thread()
+
+        try:
+            files_list = os.listdir(from_)
+            self._run_thread(from_, to_, thread)
+
+            for file_name in files_list:
+                try:
+                    self.main(f"{self.from_}/{file_name}", self.to_)
+                except FileNotFoundError:
+                    pass
+
+        except NotADirectoryError:
+            self._run_thread(from_, to_, thread)
+
+    def run_main(self, from_: List[str], to_: str, threads_amount=1) -> None:
+        self.from_ = from_
+        self.to_ = to_
+        self._threads_amount = threads_amount
+        for arg in self.from_:
+            self.main(arg, self.to_)
 
 
-def run_move_files(from_: List[str], to_: str, threads_amount: int) -> None:
-    for arg in from_:
-        move_files(arg, to_, max(threads_amount, 1))
-
-
-def get_threads_amount(arg_threads) -> int:
+def get_threads_amount(arg_threads: int) -> int:
     if arg_threads:
         return max(arg_threads, 1)
     return 1
@@ -70,9 +100,11 @@ def main() -> None:
     parser.add_argument("-t", "--TO")
     parser.add_argument("-thr", "--threads", type=int)
     args = parser.parse_args()
+    threads_amount = get_threads_amount(args.threads)
 
     if args.operation == "move":
-        run_move_files(args.FROM, args.TO, get_threads_amount(args.threads))
+        mover = FileMover(MoveFileThread, shutil.move)
+        mover.run_main(args.FROM, args.TO, threads_amount)
 
 
 if __name__ == "__main__":
